01a0e992b7d863f7fd4bbbbaebda139a288be0d3
yungblud <killi8n@gmail.com>
2024-01-08 10:52:25 +0900

    feat: header logo

--- packages/wamuseum-client/src/ui-components/Header.tsx
+++ packages/wamuseum-client/src/ui-components/Header.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
 import styles from './Header.module.css'
 
 export default function Header() {
-  return <h2 className={styles.heading}>WaMuseum</h2>
+  return (
+    <h2 className={styles.heading}>
+      <Link href="/">WaMuseum</Link>
+    </h2>
+  )
 }
